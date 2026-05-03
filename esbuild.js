8bd864326157cf389d030b053d950786c7e5b450
Angelos Petropoulos <angelos.petropoulos@microsoft.com>
2026-05-03 06:35:13 -0500

    Update bundle size baselines

--- esbuild.js
+++ esbuild.js
@@ -413,9 +413,9 @@ async function main() {
 	if (production) {
 		const BASELINES = {
 			'extension.js':                                        1229,
-			'webview/webview.bundle.js':                           2087,
+			'webview/webview.bundle.js':                           2160,
 			'webview/md-editor.bundle.js':                          146,
-			'webview/tutorial-viewer.bundle.js':                     61,
+			'webview/tutorial-viewer.bundle.js':                    154,
 			'queryEditor/vendor/echarts/echarts.webview.js':        646,
 			'queryEditor/vendor/toastui-editor/toastui-editor.webview.js': 603,
 			'monaco/':                                            11445,
